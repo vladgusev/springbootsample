f5afde8cef76a9de34b39dd80e807a6dfc976e3e
arunneoz <arunk777@yahoo.com>
2018-04-25 13:01:59 -0500

    Update HelloController.java

--- src/main/java/com/example/devops/cicd/HelloController.java
+++ src/main/java/com/example/devops/cicd/HelloController.java
@@ -3,13 +3,42 @@ package com.example.devops.cicd;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot V1.0!";
+        return "Greetings from Spring Boot!";
     }
     
+    @RequestMapping(
+    		  value = "/add", 
+    		  params = { "number1", "number2" }, 
+    		  method = RequestMethod.GET)
+    		@ResponseBody
+    		public String add(@RequestParam("number1") long number1, @RequestParam("number2") long number2)
+    		 
+    		{
+    			long result = number1+number2;
+    		    return "Addition Result is = " + result;
+    		}
+   
+    @RequestMapping(
+  		  value = "/subtract", 
+  		  params = { "number1", "number2" }, 
+  		  method = RequestMethod.GET)
+  		@ResponseBody
+  		public String subtract(@RequestParam("number1") long number1, @RequestParam("number2") long number2)
+  		 
+  		{
+    			long result = number1-number2;
+  		    return "Subtraction Result is = " + result;
+  		}
+
+    
 }
+
